ea9016747385d8ec0971fb7a29d41a96188c1316
Roland Liedtke <liedtke.roland@gmail.com>
2024-12-30 21:12:48 +0100

    Create TempSensorReader.ino

---
+++ Arduino/SENSOR/TempSensorReader.ino
@@ -0,0 +1,22 @@
+/* 
+  Arduino Temp Sensor Reader
+  Dev: Roland Liedtke
+  Date: 30/12/2024
+*/
+
+const byte tempSensor = A1;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Test czujnika temperatury");
+}
+
+
+void loop() {
+  float temp = ((analogRead(tempSensor) * 5.0) / 1024.0) * 100;
+  Serial.print("Temperatura: ");
+  Serial.print(temp);
+  Serial.println(" ℃");
+  delay(5000);
+}
