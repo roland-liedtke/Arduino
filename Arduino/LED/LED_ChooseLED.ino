f46842d36e38aab4787572c62c1c661d2cdf380a
Roland Liedtke <liedtke.roland@gmail.com>
2025-01-02 20:54:45 +0100

    Create LED_ChooseLED.ino

---
+++ Arduino/LED/LED_ChooseLED.ino
@@ -0,0 +1,55 @@
+/* 
+  Arduino 5 Led Switch
+  Dev: Roland Liedtke
+  Date: 02/01/2025
+  Version: 1
+*/
+
+int value = 0;
+
+
+void setup() {
+  pinMode(8, OUTPUT);                                                 // LEDs
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  value = analogRead(A4);                                             // Read value from ADC   
+  value = map(value, 0, 1023, 1, 5);                                  // Map Value from 0-1023 to 1-5
+
+  if (value == 1) {
+      digitalWrite(8, HIGH); 
+      digitalWrite(9, LOW); 
+      digitalWrite(10, LOW); 
+      digitalWrite(11, LOW); 
+      digitalWrite(12, LOW); 
+  } else if (value == 2) {
+      digitalWrite(8, LOW); 
+      digitalWrite(9, HIGH); 
+      digitalWrite(10, LOW); 
+      digitalWrite(11, LOW); 
+      digitalWrite(12, LOW);       
+  } else if (value == 3) {
+      digitalWrite(8, LOW); 
+      digitalWrite(9, LOW); 
+      digitalWrite(10, HIGH); 
+      digitalWrite(11, LOW); 
+      digitalWrite(12, LOW);       
+  } else if (value == 4) {
+      digitalWrite(8, LOW); 
+      digitalWrite(9, LOW); 
+      digitalWrite(10, LOW); 
+      digitalWrite(11, HIGH); 
+      digitalWrite(12, LOW);       
+  } else {
+      digitalWrite(8, LOW); 
+      digitalWrite(9, LOW); 
+      digitalWrite(10, LOW); 
+      digitalWrite(11, LOW); 
+      digitalWrite(12, HIGH);       
+  }
+  delay(50);
+}
